c0523d98da1a0f6d22cfd0cfe8c54ea172b0c2a2
Nathan Chevenard <nathan.chevenard@gmail.com>
2023-08-26 09:57:32 +0200

    Android integration

--- Scripts/Asteroid/Asteroid.gd
+++ Scripts/Asteroid/Asteroid.gd
@@ -5,13 +5,11 @@ class_name Asteroid
 @export var speed : float = 200.0
 @export var torque : float = 50.0
 
-
 func _physics_process(delta):
 	var velocity : Vector2 = speed * direction * delta
 	global_position += velocity
 	
 	rotation_degrees += torque * delta
-
 
 func _on_body_entered(body: Player):
 	body.destroy()
